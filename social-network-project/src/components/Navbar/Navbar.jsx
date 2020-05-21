96e2ffa803956d3f3bfd6a224052cb3a1509f4f7
BudakovDmitry <budakovdima7@gmail.com>
2020-05-21 21:59:56 +0300

    Create a components
    Create a Header, Navbar and Profile components

---
+++ social-network-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import classes from "./Navbar.module.css";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className={classes.nav}></nav>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ social-network-project/src/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import classes from "./Profile.module.css";
+
+const Profile = () => {
+    return (
+        <div>
+            <main>
+                
+            </main>
+        </div>
+    )
+}
+
+export default Profile;
